1fab8f607fb973439ff0e789bc159b8ddd5c00e1
wonderotter <gouk32@naver.com>
2020-11-18 16:23:32 +0900

    Polishing

--- src/test/java/me/wonderotter/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/wonderotter/demoinflearnrestapi/events/EventControllerTests.java
@@ -8,16 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -58,6 +58,8 @@ public class EventControllerTests {
                 .content(objectMapper.writeValueAsString(event))) // 받고 싶은 응답
                 .andDo(print())
                 .andExpect(status().isCreated()) //isCreated: 201
-                .andExpect(jsonPath("id").exists());
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE));
     }
 }
